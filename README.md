cd97e280ad617aa7d7249e6da5f3abb478055c51
Yogesh Gaonkar <yogeshgaonkar@Yogeshs-MacBook-Air.local>
2023-08-16 20:12:52 +0530

    Add readme

---
+++ README.md
@@ -0,0 +1,13 @@
+Full Stack web application
+
+# client
+
+implemented using react, redux
+
+# server side
+
+microservices based architecture using node js and azure
+
+# System Design
+
+![system design](https://github.com/gaonkar18y/food-delivery/blob/main/FoodDelivery.jpg)
--- services/inventory-service/src/services/azureBlob.ts
+++ services/inventory-service/src/services/azureBlob.ts
@@ -21,11 +21,10 @@ function bufferToStream(binary: Buffer) {
 const getFilename = (file: Express.Multer.File) =>`${v4()}-${Date.now()}.${file.mimetype.split('/')[1]}`;
 
 export const uploadFileToAzure = async (file: Express.Multer.File): Promise<string | undefined>=>{
-
     try{
         const blobName = getFilename(file);
         const blobService = new BlockBlobClient(process.env.AZURE_STORAGE_CONNECTION_STRING as string,"productimages",blobName);
-        const  stream = bufferToStream(file.buffer);
+        const stream = bufferToStream(file.buffer);
         const streamLength = file.buffer.length;
         await blobService.uploadStream(stream, streamLength);
         return blobService.url;
